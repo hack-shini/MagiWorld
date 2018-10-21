69d830497eb83cf1b892cc250234a42ff1813ecb
Dimitri comenda <comenda.dimitri007@gmail.com>
2018-10-21 22:56:17 +0200

    Classe Plaer -> Ajout de la création du joueur 1 et 2

--- src/com/comenda/magiWorld/Player.java
+++ src/com/comenda/magiWorld/Player.java
@@ -6,7 +6,7 @@ public class Player extends Skills {
 
     Scanner clavier = new Scanner(System.in);
 
-    protected int choiceCharacter(){
+    public int player_1(){
 
         System.out.println("Création du joueur 1");
         System.out.println("Veuillez choisir la classe de votre Héros : 1 - Guerrier, 2 - Rôdeur, 3 - Magicien");
@@ -26,7 +26,27 @@ public class Player extends Skills {
         }
 
         return choice;
+    }
 
+    public int player_2(){
 
+        System.out.println("Création du joueur 2");
+        System.out.println("Veuillez choisir la classe de votre Héros : 1 - Guerrier, 2 - Rôdeur, 3 - Magicien");
+        int choice = clavier.nextInt();
+
+        switch (choice){
+            case 1:
+                Warrior warrior = new Warrior();
+                break;
+            case 2:
+                Prowler prowler = new Prowler();
+                break;
+            case 3:
+                Wizard wizzard = new Wizard();
+                break;
+
+        }
+
+        return choice;
     }
 }
